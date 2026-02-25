9dfb000612f9c14bd837e2f0e72dbce58d065dd4
Jarek Radosz <jarek@cvx.dev>
2026-02-25 12:10:18 +0100

    add new modules and change formatting

--- modules/select-kit.js
+++ modules/select-kit.js
@@ -1,55 +1,74 @@
 export default {
   components: {
-    "bulk-select-bookmarks-dropdown":
-      "select-kit/components/bulk-select-bookmarks-dropdown",
-    "categories-admin-dropdown":
-      "select-kit/components/categories-admin-dropdown",
+    "bulk-select-bookmarks-dropdown": "select-kit/components/bulk-select-bookmarks-dropdown",
+    "categories-admin-dropdown": "select-kit/components/categories-admin-dropdown",
     "category-chooser": "select-kit/components/category-chooser",
-    "category-drop-more-collection":
-      "select-kit/components/category-drop-more-collection",
+    "category-drop-more-collection": "select-kit/components/category-drop-more-collection",
     "category-drop": "select-kit/components/category-drop",
-    "category-notifications-button":
-      "select-kit/components/category-notifications-button",
+    "category-drop/category-drop-header": "select-kit/components/category-drop/category-drop-header",
+    "category-notifications-button": "select-kit/components/category-notifications-button",
     "category-row": "select-kit/components/category-row",
     "category-selector": "select-kit/components/category-selector",
+    "color-palette-picker": "select-kit/components/color-palette-picker",
+    "color-palette-picker/color-palette-picker-row": "select-kit/components/color-palette-picker/color-palette-picker-row",
     "color-palettes": "select-kit/components/color-palettes",
+    "color-palettes/color-palettes-row": "select-kit/components/color-palettes/color-palettes-row",
     "combo-box": "select-kit/components/combo-box",
+    "combo-box/combo-box-header": "select-kit/components/combo-box/combo-box-header",
     "composer-actions": "select-kit/components/composer-actions",
     "create-color-row": "select-kit/components/create-color-row",
     "dropdown-select-box": "select-kit/components/dropdown-select-box",
-    "email-group-user-chooser-filter":
-      "select-kit/components/email-group-user-chooser-filter",
-    "email-group-user-chooser-row":
-      "select-kit/components/email-group-user-chooser-row",
-    "email-group-user-chooser":
-      "select-kit/components/email-group-user-chooser",
+    "dropdown-select-box/dropdown-select-box-header": "select-kit/components/dropdown-select-box/dropdown-select-box-header",
+    "dropdown-select-box/dropdown-select-box-row": "select-kit/components/dropdown-select-box/dropdown-select-box-row",
+    "email-group-user-chooser-filter": "select-kit/components/email-group-user-chooser-filter",
+    "email-group-user-chooser-row": "select-kit/components/email-group-user-chooser-row",
+    "email-group-user-chooser": "select-kit/components/email-group-user-chooser",
     "filter-for-more": "select-kit/components/filter-for-more",
     "flair-chooser": "select-kit/components/flair-chooser",
     "flair-row": "select-kit/components/flair-row",
     "font-selector": "select-kit/components/font-selector",
+    "font-selector/font-selector-row": "select-kit/components/font-selector/font-selector-row",
     "form-template-chooser": "select-kit/components/form-template-chooser",
-    "future-date-input-selector":
-      "select-kit/components/future-date-input-selector",
+    "future-date-input-selector": "select-kit/components/future-date-input-selector",
+    "future-date-input-selector/future-date-input-selector-header": "select-kit/components/future-date-input-selector/future-date-input-selector-header",
+    "future-date-input-selector/future-date-input-selector-row": "select-kit/components/future-date-input-selector/future-date-input-selector-row",
     "group-chooser": "select-kit/components/group-chooser",
     "group-dropdown": "select-kit/components/group-dropdown",
-    "group-notifications-button":
-      "select-kit/components/group-notifications-button",
+    "group-notifications-button": "select-kit/components/group-notifications-button",
     "homepage-style-selector": "select-kit/components/homepage-style-selector",
+    "homepage-style-selector/homepage-style-selector-row": "select-kit/components/homepage-style-selector/homepage-style-selector-row",
     "icon-picker": "select-kit/components/icon-picker",
     "list-setting": "select-kit/components/list-setting",
     "mini-tag-chooser": "select-kit/components/mini-tag-chooser",
+    "mini-tag-chooser/selected-collection": "select-kit/components/mini-tag-chooser/selected-collection",
     "multi-select": "select-kit/components/multi-select",
+    "multi-select/format-selected-content": "select-kit/components/multi-select/format-selected-content",
+    "multi-select/multi-select-filter": "select-kit/components/multi-select/multi-select-filter",
+    "multi-select/multi-select-header": "select-kit/components/multi-select/multi-select-header",
+    "multi-select/selected-category": "select-kit/components/multi-select/selected-category",
+    "multi-select/selected-color": "select-kit/components/multi-select/selected-color",
     "none-category-row": "select-kit/components/none-category-row",
     "notifications-button": "select-kit/components/notifications-button",
+    "notifications-button/notifications-button-row": "select-kit/components/notifications-button/notifications-button-row",
     "notifications-filter": "select-kit/components/notifications-filter",
+    "notifications-filter/notifications-filter-header": "select-kit/components/notifications-filter/notifications-filter-header",
     "period-chooser": "select-kit/components/period-chooser",
+    "period-chooser/period-chooser-header": "select-kit/components/period-chooser/period-chooser-header",
+    "period-chooser/period-chooser-row": "select-kit/components/period-chooser/period-chooser-row",
     "pinned-button": "select-kit/components/pinned-button",
     "pinned-options": "select-kit/components/pinned-options",
-    "search-advanced-category-chooser":
-      "select-kit/components/search-advanced-category-chooser",
+    "search-advanced-category-chooser": "select-kit/components/search-advanced-category-chooser",
     "select-kit": "select-kit/components/select-kit",
-    "selected-choice-category":
-      "select-kit/components/selected-choice-category",
+    "select-kit/errors-collection": "select-kit/components/select-kit/errors-collection",
+    "select-kit/select-kit-body": "select-kit/components/select-kit/select-kit-body",
+    "select-kit/select-kit-collection": "select-kit/components/select-kit/select-kit-collection",
+    "select-kit/select-kit-create-row": "select-kit/components/select-kit/select-kit-create-row",
+    "select-kit/select-kit-filter": "select-kit/components/select-kit/select-kit-filter",
+    "select-kit/select-kit-header": "select-kit/components/select-kit/select-kit-header",
+    "select-kit/select-kit-none-row": "select-kit/components/select-kit/select-kit-none-row",
+    "select-kit/select-kit-row": "select-kit/components/select-kit/select-kit-row",
+    "select-kit/single-select-header": "select-kit/components/select-kit/single-select-header",
+    "selected-choice-category": "select-kit/components/selected-choice-category",
     "selected-choice-color": "select-kit/components/selected-choice-color",
     "selected-choice": "select-kit/components/selected-choice",
     "selected-color": "select-kit/components/selected-color",
@@ -60,85 +79,22 @@ export default {
     "tag-chooser-row": "select-kit/components/tag-chooser-row",
     "tag-chooser": "select-kit/components/tag-chooser",
     "tag-drop": "select-kit/components/tag-drop",
+    "tag-drop/tag-drop-header": "select-kit/components/tag-drop/tag-drop-header",
+    "tag-dropdown": "select-kit/components/tag-dropdown",
     "tag-group-chooser": "select-kit/components/tag-group-chooser",
-    "tag-notifications-button":
-      "select-kit/components/tag-notifications-button",
+    "tag-notifications-button": "select-kit/components/tag-notifications-button",
     "tag-row": "select-kit/components/tag-row",
-    "tags-intersection-chooser":
-      "select-kit/components/tags-intersection-chooser",
+    "tags-intersection-chooser": "select-kit/components/tags-intersection-chooser",
     "timezone-input": "select-kit/components/timezone-input",
-    "toolbar-popup-menu-options":
-      "select-kit/components/toolbar-popup-menu-options",
+    "toolbar-popup-menu-options": "select-kit/components/toolbar-popup-menu-options",
+    "toolbar-popup-menu-options/toolbar-popup-menu-options-heading": "select-kit/components/toolbar-popup-menu-options/toolbar-popup-menu-options-heading",
     "topic-chooser": "select-kit/components/topic-chooser",
-    "topic-notifications-button":
-      "select-kit/components/topic-notifications-button",
-    "topic-notifications-options":
-      "select-kit/components/topic-notifications-options",
+    "topic-notifications-button": "select-kit/components/topic-notifications-button",
+    "topic-notifications-options": "select-kit/components/topic-notifications-options",
     "topic-row": "select-kit/components/topic-row",
     "user-chooser": "select-kit/components/user-chooser",
-    "user-notifications-dropdown":
-      "select-kit/components/user-notifications-dropdown",
-    "watched-words": "select-kit/components/watched-words",
     "user-chooser/user-row": "select-kit/components/user-chooser/user-row",
-    "toolbar-popup-menu-options/toolbar-popup-menu-options-heading":
-      "select-kit/components/toolbar-popup-menu-options/toolbar-popup-menu-options-heading",
-    "tag-drop/tag-drop-header":
-      "select-kit/components/tag-drop/tag-drop-header",
-    "select-kit/errors-collection":
-      "select-kit/components/select-kit/errors-collection",
-    "select-kit/select-kit-body":
-      "select-kit/components/select-kit/select-kit-body",
-    "select-kit/select-kit-collection":
-      "select-kit/components/select-kit/select-kit-collection",
-    "select-kit/select-kit-create-row":
-      "select-kit/components/select-kit/select-kit-create-row",
-    "select-kit/select-kit-filter":
-      "select-kit/components/select-kit/select-kit-filter",
-    "select-kit/select-kit-header":
-      "select-kit/components/select-kit/select-kit-header",
-    "select-kit/select-kit-none-row":
-      "select-kit/components/select-kit/select-kit-none-row",
-    "select-kit/select-kit-row":
-      "select-kit/components/select-kit/select-kit-row",
-    "select-kit/single-select-header":
-      "select-kit/components/select-kit/single-select-header",
-    "period-chooser/period-chooser-header":
-      "select-kit/components/period-chooser/period-chooser-header",
-    "period-chooser/period-chooser-row":
-      "select-kit/components/period-chooser/period-chooser-row",
-    "notifications-filter/notifications-filter-header":
-      "select-kit/components/notifications-filter/notifications-filter-header",
-    "notifications-button/notifications-button-row":
-      "select-kit/components/notifications-button/notifications-button-row",
-    "multi-select/format-selected-content":
-      "select-kit/components/multi-select/format-selected-content",
-    "multi-select/multi-select-filter":
-      "select-kit/components/multi-select/multi-select-filter",
-    "multi-select/multi-select-header":
-      "select-kit/components/multi-select/multi-select-header",
-    "multi-select/selected-category":
-      "select-kit/components/multi-select/selected-category",
-    "multi-select/selected-color":
-      "select-kit/components/multi-select/selected-color",
-    "mini-tag-chooser/selected-collection":
-      "select-kit/components/mini-tag-chooser/selected-collection",
-    "homepage-style-selector/homepage-style-selector-row":
-      "select-kit/components/homepage-style-selector/homepage-style-selector-row",
-    "future-date-input-selector/future-date-input-selector-header":
-      "select-kit/components/future-date-input-selector/future-date-input-selector-header",
-    "future-date-input-selector/future-date-input-selector-row":
-      "select-kit/components/future-date-input-selector/future-date-input-selector-row",
-    "font-selector/font-selector-row":
-      "select-kit/components/font-selector/font-selector-row",
-    "dropdown-select-box/dropdown-select-box-header":
-      "select-kit/components/dropdown-select-box/dropdown-select-box-header",
-    "dropdown-select-box/dropdown-select-box-row":
-      "select-kit/components/dropdown-select-box/dropdown-select-box-row",
-    "combo-box/combo-box-header":
-      "select-kit/components/combo-box/combo-box-header",
-    "color-palettes/color-palettes-row":
-      "select-kit/components/color-palettes/color-palettes-row",
-    "category-drop/category-drop-header":
-      "select-kit/components/category-drop/category-drop-header",
+    "user-notifications-dropdown": "select-kit/components/user-notifications-dropdown",
+    "watched-words": "select-kit/components/watched-words",
   },
 };
